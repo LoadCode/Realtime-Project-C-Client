79f5241e171db18b6436aa2c210f58577a0b0ce6
Julio César Echeverri Marulanda <julio.em7@gmail.com>
2017-04-16 15:14:21 -0500

    Cliente en C que envía el valor de un contador...funciona correctamente

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <math.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <uqeasysocket.h>
 
 #define IP_SERVER "192.168.0.20"
 #define PORT 2222
 #define REQUEST_VAL 45862
 #define REQUEST_YES 2958
-#define TOL 0.000001
 
 struct socket_info
 {
@@ -24,12 +24,12 @@ int main()
 {
 	socket_info_t client_info;
 	int request = REQUEST_VAL;
-	int req_answer, i = 1;
-	int vector_length = 50;
-	int rev_vec_len;
-	double vector_vals[vector_length];
-	double rev_vec_vals[vector_length];
-	double norm_result = 0.0, norm_result_server;
+	int req_answer;
+	int finish = 0;
+	double i = 0, i_swap = 0;
+	int Ts = 5;
+	
+	
 
 	// creates client
 	if(client_create(IP_SERVER, PORT, &client_info.server_socket))
@@ -40,64 +40,30 @@ int main()
 
 	// try to connect with server
 	swapbytes(&request, sizeof(int));
+	printf("enviando solicitud\n");
+	send_data(client_info.server_socket, &request, sizeof(int));
+	receive_data(client_info.server_socket, &req_answer, sizeof(int));
+	swapbytes(&req_answer, sizeof(int));
+	printf("Respuesta request: %d\n",req_answer);
+	if(req_answer != REQUEST_YES)
+		return 0;
+
+	receive_data(client_info.server_socket, &Ts, sizeof(int));
+	swapbytes(&Ts, sizeof(int));
+	printf("Ts = %d\n",Ts);
 	do
 	{
-		printf("enviando solicitud\n");
-		send_data(client_info.server_socket, &request, sizeof(int));
-		//printf("recibiendo respuesta\n");
-		receive_data(client_info.server_socket, &req_answer, sizeof(int));
-		swapbytes(&req_answer, sizeof(int));
-		printf("Respuesta request: %d\n",req_answer);
-		printf("intento: %d\n",i);
+		swapbytes(&i_swap, sizeof(double));
+		send_data(client_info.server_socket, &i_swap, sizeof(double));
+		receive_data(client_info.server_socket, &finish, sizeof(int));
+		swapbytes(&finish, sizeof(int));
 		i++;
-	}while(req_answer != REQUEST_YES);
-
-	// send length of vector
-	rev_vec_len = vector_length;
-	swapbytes(&rev_vec_len, sizeof(int));
-	send_data(client_info.server_socket, &rev_vec_len, sizeof(int));
-
-	// generate random vector
-	for(int i =0; i<vector_length; i++)
-	{
-		vector_vals[i] = (float)rand()/(float)(RAND_MAX/50) +i;
-		rev_vec_vals[i] = vector_vals[i];
-		swapbytes(&rev_vec_vals[i], sizeof(double));
-	}
-
-	// send vector elements
-	for(int i =0; i < vector_length;  i++)
-		send_data(client_info.server_socket, &rev_vec_vals[i], sizeof(double));
-
-	// compute the vector norm
-	//norm_result = norm(vector_vals, vector_length);
-	for(int i = 0;i<vector_length; i++)
-		norm_result += vector_vals[i] * vector_vals[i];
-	norm_result = sqrt(norm_result);
-
-	receive_data(client_info.server_socket, &norm_result_server, sizeof(double));
-	swapbytes(&norm_result_server, sizeof(double));
-
-	// compare answers
-	if(abs(norm_result-norm_result_server) < TOL)
-		printf("The result difference is less than %g\n",TOL);
-	else
-		printf("The result difference is greater than %g\n",TOL);
-
-	// shows answer
-	printf("Local Norm: %g\n",norm_result);
-	printf("Server Norm: %g\n",norm_result_server);
+		i_swap = i;
+		printf("contanto i = %f\n",i);
+		sleep(Ts);
+	}while(finish == 0);
+	printf("Proceso de cliente terminado bien\n");
+	close_socket(client_info.server_socket);
 
 	return 0;
 }
-
-
-
-double norm(double * array, int size)
-{
-	double sum = 0;
-	for(int i = 0; i < size; i++)
-		sum += array[i]*array[i];
-	return sqrt(sum);
-}
-
