55a3f8b5b2f6c1de434625a537fe913192d56a2b
Julio César Echeverri Marulanda <julio.em7@gmail.com>
2017-04-16 16:08:52 -0500

    Se recibe el Ts en formato Double y se usa la función nanosleep, el hilo se interrumpe correctamente y los sockets se cierran bien

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <math.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <time.h>
 #include <uqeasysocket.h>
 
 #define IP_SERVER "192.168.0.20"
@@ -15,19 +15,25 @@ struct socket_info
 };
 
 typedef struct socket_info socket_info_t;
+typedef struct timespec time_process_t;
 
+typedef struct 
+{
+	int usbPort;
+	double normal_ts;
+	time_process_t sample_time;	
+} process_data_t;
 
-double norm(double * array, int size);
-
+time_process_t get_time_struct(double Ts);
 
 int main()
 {
 	socket_info_t client_info;
+	process_data_t process_info;
 	int request = REQUEST_VAL;
 	int req_answer;
 	int finish = 0;
 	double i = 0, i_swap = 0;
-	int Ts = 5;
 	
 	
 
@@ -47,10 +53,18 @@ int main()
 	printf("Respuesta request: %d\n",req_answer);
 	if(req_answer != REQUEST_YES)
 		return 0;
-
-	receive_data(client_info.server_socket, &Ts, sizeof(int));
-	swapbytes(&Ts, sizeof(int));
-	printf("Ts = %d\n",Ts);
+	
+	// Obteniendo parámetros
+	receive_data(client_info.server_socket, &process_info.usbPort, sizeof(int));
+	swapbytes(&process_info.usbPort, sizeof(int));
+	receive_data(client_info.server_socket, &process_info.normal_ts, sizeof(double));
+	swapbytes(&process_info.normal_ts, sizeof(double));
+	
+	printf("Ts = %f\n",process_info.normal_ts);
+	printf("usbPort = %d\n",process_info.usbPort);
+	process_info.sample_time = get_time_struct(process_info.normal_ts);
+	
+	// Muestreo del proceso
 	do
 	{
 		swapbytes(&i_swap, sizeof(double));
@@ -60,10 +74,23 @@ int main()
 		i++;
 		i_swap = i;
 		printf("contanto i = %f\n",i);
-		sleep(Ts);
+		nanosleep(&process_info.sample_time, NULL);
 	}while(finish == 0);
 	printf("Proceso de cliente terminado bien\n");
 	close_socket(client_info.server_socket);
 
 	return 0;
 }
+
+
+time_process_t get_time_struct(double Ts)
+{
+	time_process_t time;
+	double seconds, nanoseconds;
+	nanoseconds  = modf(Ts, &seconds) * 1000000000UL; // gets seconds and nanoseconds from incomming millis
+	time.tv_sec  = seconds;
+	time.tv_nsec = nanoseconds;
+	return time;
+}
+
+
